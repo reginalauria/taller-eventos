24d81303ae88148f082a43faaab83f0033020117
regina <reginalauria24@gmail.com>
2024-08-21 23:32:02 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -3,5 +3,11 @@ const divs=document.querySelectorAll("div");
 for (const div of divs) {
 div.addEventListener("click", function () {
 alert ("Hola! Soy el div");
-});    
+});
+
+const button = div.querySelectorAll("input=[type='submit']");
+
+button.addEventListener ("click", function (event) {
+event.stopPropagation ();
+});
 }
